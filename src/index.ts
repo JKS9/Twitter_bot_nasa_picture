41a83fab20dea5bef2ff35c929c9d8b59fe80868
JKS9 <juzans.etienne@gmail.com>
2022-10-24 14:21:52 +0200

    update cron time

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {CronJob} from 'cron';
 import TweetController from './controller/tweet';
 
 const hour = '0 */6 * * *';
-const minute = '*/10 * * * *';
+const minute = '*/10 * * * * *';
 
 const job: CronJob = new CronJob(minute, () => {
   TweetController();
